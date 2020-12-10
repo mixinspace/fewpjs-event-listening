5fb55c78362371922a80311c5799f1a7b27b2539
mixinspace <islammadkaur@gmail.com>
2020-12-10 11:58:52 -0600

    commit tree 1f4937aecb6f4bec3cd6a2477c1ed88fc8de6daf

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
-}
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//   input.addEventListener('click', function(event) {
+//     return "clicked"
+//   });
+// }
+
+
+const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+}); 
